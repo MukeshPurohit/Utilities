126bdd617c9041f78fcc5244083fa3814f69f332
MukeshPurohit <purohit.mukesh@gmail.com>
2018-09-05 09:32:58 +0000

    First commit

--- Emulator_CICD.bat
+++ Emulator_CICD.bat
@@ -1,3 +1,41 @@
+node {
+
+   dir('C:/Users/capeuser/AppData/Local/Android/Sdk/tools/bin')
+   {    
+       bat(/echo y | sdkmanager --update/)
+       bat(/echo y | sdkmanager system-images;android-25;google_apis;x86/)
+       bat(/echo y | sdkmanager --licenses/)
+       bat(/echo y | avdmanager create avd -f --name "processing-phone-%BUILD_NUMBER%" --package "system-images;android-25;google_apis;x86" --abi "google_apis\/x86" -d "Nexus 5"/)
+       bat 'echo y | avdmanager list avd'
+   }
+   dir('C:/Users/capeuser/AppData/Local/Android/Sdk/tools')
+   {    
+       bat(/START \/B C:\/Users\/capeuser\/AppData\/Local\/Android\/Sdk\/tools\/emulator -avd "processing-phone-%BUILD_NUMBER%" -netspeed full -gpu off -netdelay none -verbose -ports 5584,5585 -wipe-data -no-boot-anim -no-audio -skin 480x800 -memory 2048/)
+   }
+    
+   dir('C:\\Users\\capeuser\\AppData\\Local\\Android\\Sdk\\platform-tools')
+   {    
+        bat(/adb wait-for-device/)       
+        sleep 100
+
+        bat(/adb devices/)
+        sleep 120
+
+    bat(/adb -s "emulator-5584" shell input keyevent 82/)   
+    sleep 100
+
+    bat(/adb -s "emulator-5584" install "C:\\projects\\android-sunflower\\app\\build\\outputs\\apk\\debug\\app-debug.apk"/)            
+    sleep 100    
+
+    bat(/adb shell pm list packages/)                
+    }
+    sleep 100
+}
+
+
+       
+
+	   
 cd %ANDROID_HOME%\tools\bin
 
 set Number=%BUILD_NUMBER%
